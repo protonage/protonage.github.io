3d43a8ccd2e773d8f0eefcf20dadaff160830267
proton <lolfile@protonmail.ch>
2024-06-06 14:30:08 -0500

    bruh

--- virtualgrass/script.lua
+++ virtualgrass/script.lua
@@ -1,7 +1,7 @@
 local grass = get("grass")
 local gb = get("gb")
 local timesPressed = 0
-grass.on_click(function()
+get("grass").on_click(function()
     timesPressed = timesPressed + 1
-    gb.set_content("You have pressed the grass " + timesPressed + " times.")
+    get("gb").set_content("You have pressed the grass " + timesPressed + " times.")
 end)
