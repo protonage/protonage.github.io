86c44cd175ff3634038afc43faad3580331d772a
proton <lolfile@protonmail.ch>
2024-06-06 14:33:14 -0500

    i hate my life

--- virtualgrass/script.lua
+++ virtualgrass/script.lua
@@ -1,7 +1,9 @@
 local grass = get("grass")
 local gb = get("gb")
 local timesPressed = 0
-get("grass").on_click(function()
-    timesPressed = timesPressed + 1
-    get("gb").set_content("You have pressed the grass " + timesPressed + " times.")
-end)
+
+local function touchgrass()
+    timesPressed = timesPressed +1
+    get("gb").set_content("You have touched grass " + timesPressed + " times.")
+end
+get("grass").on_click(touchgrass)
