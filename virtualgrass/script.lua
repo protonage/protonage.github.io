faea7b22f6587befe73ef9321dff9c0bccb6f2b5
proton <lolfile@protonmail.ch>
2024-06-06 14:34:54 -0500

    debug thifnaklasnfglfasdgjklsdfhgk,jhxcghsdfjkl

--- virtualgrass/script.lua
+++ virtualgrass/script.lua
@@ -3,7 +3,8 @@ local gb = get("gb")
 local timesPressed = 0
 
 local function touchgrass()
-    timesPressed = timesPressed +1
+    timesPressed = timesPressed + 1
     get("gb").set_content("You have touched grass " + timesPressed + " times.")
+    print("touched grass")
 end
 get("grass").on_click(touchgrass)
