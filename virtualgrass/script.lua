2279a3e0ba37c4674aa7735238735732bb03d988
proton <lolfile@protonmail.ch>
2024-06-06 15:45:15 -0500

    i dont know lua lol

--- virtualgrass/script.lua
+++ virtualgrass/script.lua
@@ -6,7 +6,7 @@ local timesPressed = 0
 
 local function touchgrass()
     timesPressed = timesPressed + 1
-    get("gb").set_content("You have touched grass " + timesPressed + " times.")
+    get("gb").set_content("You have touched grass " .. timesPressed .. " times.")
     print("touched grass")
 end
 get("grass").on_click(touchgrass)
