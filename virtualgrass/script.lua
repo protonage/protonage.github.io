3c2d363df3aad7be3dcf5cdcd49f5da0773e6cd0
proton <lolfile@protonmail.ch>
2024-06-06 14:16:43 -0500

    lets hope this works

--- virtualgrass/script.lua
+++ virtualgrass/script.lua
@@ -1,4 +1,7 @@
 local grass = get("grass")
+local gb = get("gb")
+local timesPressed = 0
 grass.on_click(function()
-    print("hello!")
+    timesPressed = timesPressed + 1
+    gb.set_content("You have pressed the grass " + timesPressed + " times.")
 end)
