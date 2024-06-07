b330e19d123fe7882ebc73020bc6c5f1788144ae
proton <lolfile@protonmail.ch>
2024-06-06 23:23:19 -0500

    typos

--- oddit/script.lua
+++ oddit/script.lua
@@ -1,8 +1,8 @@
 get("button").on_click(function()
-    local input = get("input").get_contents()
+    local input = get("input").get_content()
     if (number % 2 == 0) then
-        get("output").set_contents("Even")
+        get("output").set_content("Even")
     else
-        get("output").set_contents("Odd")
+        get("output").set_content("Odd")
     end
 end)
