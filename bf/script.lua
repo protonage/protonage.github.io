1302b73e63f5cf79d3862bd53d5852da72bccf29
proton <lolfile@protonmail.ch>
2024-06-07 00:22:46 -0500

    debug measures

--- bf/script.lua
+++ bf/script.lua
@@ -1,11 +1,14 @@
 wait(0.1)
-
+print("1")
 local function cbclick()
     print("cb has been clicked")
 end
+print("2")
 local function ibclick()
     print("ib has been clicked")
 end
-
+print("3")
 get("cb").on_click()
+print("4")
 get("ib").on_click()
+print("5")
