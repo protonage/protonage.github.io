4136efeabcba5598a7d2876c9b94a1a3baaf39ee
proton <lolfile@protonmail.ch>
2024-06-06 23:50:18 -0500

    lets hope this works

--- oddit/script.lua
+++ oddit/script.lua
@@ -1,5 +1,5 @@
 get("button").on_click(function()
-    local input = get("input").get_content()
+    local input = string.sub(get("input").get_content(), -1)
     if (input % 2 == 0) then
         get("output").set_content("Even")
     else
